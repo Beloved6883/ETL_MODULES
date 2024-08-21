6ed2fcee11f85d03d832cfcfd3ce2aa8e3b4b5a8
Natasha Wilson <natasha@originsconvo.com>
2024-08-20 21:28:09 -0400

    Updated transform_social_security_names.py to import Ingestion from the module data_ingestion

--- transform_social_security_names.py
+++ transform_social_security_names.py
@@ -1,3 +1,10 @@
+import os
+project_path = os.getenv('PROJECT_PATH')
+import sys
+sys.path.append(project_path) # This is necessary to be able to import a module from a different directory
+import pandas as pd
+from data_cleaning_classes.data_ingestion import Ingestion
+
 path = '/Users/beloved683/Desktop/Programming/ETL_MODULES/social_security_data/names/'
 
 file_names = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
@@ -16,6 +23,6 @@ final_df = pd.concat([df.set_axis(col, axis=1) for df in temp_df_list], sort=Fal
 
 # test_df = test.function_to_use()()
 
-print(final_df.shape)
+print(final_df.head(10))
 
 # print(file_names)
