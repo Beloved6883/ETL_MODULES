3f7a4a9756aa6200ccd95d457cca793bf927f778
Natasha Wilson <natasha@originsconvo.com>
2024-08-20 22:15:54 -0400

    Updated transform_social_security_names.py

--- transform_social_security_names.py
+++ transform_social_security_names.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append(project_path) # This is necessary to be able to import a module from a different directory
 import pandas as pd
 from data_cleaning_classes.data_ingestion import Ingestion
+import re
 
 path = '/Users/beloved683/Desktop/Programming/ETL_MODULES/social_security_data/names/'
 
@@ -13,10 +14,14 @@ temp_df_list = []
 
 for file in file_names:
     file_base, file_ext = os.path.splitext(file)
-    temp = Ingestion(file_path = path + file, extension = file_ext, header = 0, converters={'score':int})
+    match = re.search(r'\d{4}',file_base) 
+    year = match.group()
+    columns_names = ['first_name', 'sex', 'frequency']
+    temp = Ingestion(file_path = path + file, extension = file_ext, header = 0, converters={'score':int}, names=columns_names)
     temp_df = temp.function_to_use()()
+    temp_df['year'] = year
     temp_df_list.append(temp_df)
 col = temp_df_list[0].columns
-final_df = pd.concat([df.set_axis(col, axis=1) for df in temp_df_list], sort=False)
-    
-print(final_df.head(10))
+final_df = pd.concat([df.set_axis(col, axis=1) for df in temp_df_list], sort=False).sort_values(by='year', ignore_index=True, ascending=True).reset_index()
+
+final_df.to_csv('/Users/beloved683/Desktop/Programming/ETL_MODULES/social_security_data/test_names.csv', index=False)
